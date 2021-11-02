f60b08c11069969ae0718667a5ca93aa2e5d66ba
Laurentiu <lcostin40@gmail.com>
2021-11-02 12:37:54 +0100

    Aggiunto array di oggetti

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+const vueApp = new Vue({
+
+    el: "#data",
+
+    data: {
+
+        tasks: [
+
+            "Studiare",
+            "Fare esercizi",
+            "Riposare",
+            "Giocare"
+        ]
+    }
+})
